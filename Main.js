92594720e43d7bb755486118504ce3a205ed07f1
ItsEstherhere <158601532+ItsEstherhere@users.noreply.github.com>
2024-04-26 17:28:42 +0100

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,20 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+function draw() {
+}
+
+function take_snapshot(){
+    save('myFilterImage.png');
+}
+
+function setup() 
+{
+  createCanvas(100,100);
+  
+  background(200);
+}
